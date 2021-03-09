9dadccb7e23d6b78bf513d1d4632c8de1aeca35e
Russell Patterson <rwp.cpe@gmail.com>
2021-03-08 21:25:38 -0500

    Initial commit of old Renamer application.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFilename = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtOldFilename = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewFilename = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFilename
+            // 
+            this.lblFilename.AutoSize = true;
+            this.lblFilename.Location = new System.Drawing.Point(27, 13);
+            this.lblFilename.Name = "lblFilename";
+            this.lblFilename.Size = new System.Drawing.Size(49, 13);
+            this.lblFilename.TabIndex = 0;
+            this.lblFilename.Text = "Directory";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(82, 10);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(236, 20);
+            this.txtDirectory.TabIndex = 1;
+            // 
+            // txtOldFilename
+            // 
+            this.txtOldFilename.Location = new System.Drawing.Point(82, 36);
+            this.txtOldFilename.Name = "txtOldFilename";
+            this.txtOldFilename.Size = new System.Drawing.Size(236, 20);
+            this.txtOldFilename.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Old Filename";
+            // 
+            // txtNewFilename
+            // 
+            this.txtNewFilename.Location = new System.Drawing.Point(82, 62);
+            this.txtNewFilename.Name = "txtNewFilename";
+            this.txtNewFilename.Size = new System.Drawing.Size(236, 20);
+            this.txtNewFilename.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "New Filename";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(324, 8);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(243, 88);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Do It.";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 117);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtNewFilename);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOldFilename);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.lblFilename);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFilename;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.TextBox txtOldFilename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewFilename;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (txtDirectory.Text == string.Empty)
+                MessageBox.Show("You must provide a directory!");
+            else if (txtOldFilename.Text == string.Empty)
+                MessageBox.Show("You must provide an old filename!");
+            else if (txtNewFilename.Text == string.Empty)
+                MessageBox.Show("You must provide an new filename!");
+            else
+                foreach (string f in Directory.GetFiles(txtDirectory.Text))
+                    if (f.Substring(f.LastIndexOf('\\') + 1).Contains(txtOldFilename.Text))
+                        File.Move(f, f.Replace(txtOldFilename.Text, txtNewFilename.Text));
+        }
+    }
+}
